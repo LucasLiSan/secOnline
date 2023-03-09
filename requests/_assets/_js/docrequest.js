406c68e6b761b7bef300531e68adc0b67f49c2d5
LucasLiSan <lukassantana_rgt@hotmail.com>
2023-03-09 12:36:23 -0300

    add js, change pictures

---
+++ requests/_assets/_js/docrequest.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $("#Bt1").mouseenter(function(){
+        $("#request-choice").attr("src", "../db_imgs/_pictures/contract.png");
+    });
+    $("#Bt1").mouseleave(function(){
+        $("#request-choice").attr("src", "../db_imgs/_pictures/test.png");
+    });
+});
+
+$(document).ready(function(){
+    $("#Bt2").mouseenter(function(){
+        $("#request-choice").attr("src", "../db_imgs/_pictures/education.png");
+    });
+    $("#Bt2").mouseleave(function(){
+        $("#request-choice").attr("src", "../db_imgs/_pictures/test.png");
+    });
+});
+
+$(document).ready(function(){
+    $("#Bt3").mouseenter(function(){
+        $("#request-choice").attr("src", "../db_imgs/_pictures/school-bag.png");
+    });
+    $("#Bt3").mouseleave(function(){
+        $("#request-choice").attr("src", "../db_imgs/_pictures/test.png");
+    });
+});
+
+$(document).ready(function(){
+    $("#Bt4").mouseenter(function(){
+        $("#request-choice").attr("src", "../db_imgs/_pictures/biology.png");
+    });
+    $("#Bt4").mouseleave(function(){
+        $("#request-choice").attr("src", "../db_imgs/_pictures/test.png");
+    });
+});
+
+$(document).ready(function(){
+    $("#Bt5").mouseenter(function(){
+        $("#request-choice").attr("src", "../db_imgs/_pictures/scholarship.png");
+    });
+    $("#Bt5").mouseleave(function(){
+        $("#request-choice").attr("src", "../db_imgs/_pictures/test.png");
+    });
+});
