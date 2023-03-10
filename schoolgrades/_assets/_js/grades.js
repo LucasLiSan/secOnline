328e095f99abef7e692ebc7b9dbaf913f39f6156
LucasLiSan <lukassantana_rgt@hotmail.com>
2023-03-10 14:35:02 -0300

    updating

---
+++ schoolgrades/_assets/_js/grades.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $(".bim1").mouseenter(function(){
+        $("#bim-choice").attr("src", "../db_imgs/_pictures/bim1.png");
+    });
+    $(".bim1").mouseleave(function(){
+        $("#bim-choice").attr("src", "../db_imgs/_pictures/bim4.png");
+    });
+});
+
+$(document).ready(function(){
+    $(".bim2").mouseenter(function(){
+        $("#bim-choice").attr("src", "../db_imgs/_pictures/bim2.png");
+    });
+    $(".bim2").mouseleave(function(){
+        $("#bim-choice").attr("src", "../db_imgs/_pictures/bim4.png");
+    });
+});
+
+$(document).ready(function(){
+    $(".bim3").mouseenter(function(){
+        $("#bim-choice").attr("src", "../db_imgs/_pictures/bim3.png");
+    });
+    $(".bim3").mouseleave(function(){
+        $("#bim-choice").attr("src", "../db_imgs/_pictures/bim4.png");
+    });
+});
+
+$(document).ready(function(){
+    $(".bim4").mouseenter(function(){
+        $("#bim-choice").attr("src", "../db_imgs/_pictures/bim4.png");
+    });
+    $(".bim4").mouseleave(function(){
+        $("#bim-choice").attr("src", "../db_imgs/_pictures/bim4.png");
+    });
+});
+
