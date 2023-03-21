59c46e6c517b7780828682b93074b94100a988fc
LucasLiSan <lukassantana_rgt@hotmail.com>
2023-03-21 17:16:42 -0300

    start php

--- info/infostudent.php
+++ info/infostudent.php
@@ -18,6 +18,14 @@ Written by:
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
             <link rel="stylesheet" href="_assets/_css/infostudent.css">
+            <?php
+                $host = 'localhost';
+                $user = 'root';
+                $pass = '';
+                $db = 'pgdatabase';
+
+                $mysqli = new mysqli($host, $user, $pass, $db);         
+            ?>
             <title>Prontuário Digital - Informação do aluno</title>
             <script src="_assets/_js/infostudent.js" defer></script>
         </head>
