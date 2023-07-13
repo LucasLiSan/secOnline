645339e4cc748dc35ead48aa599a7fa3c59486d1
LucasLiSan <lukassantana_rgt@hotmail.com>
2023-07-13 18:00:49 -0300

    update

--- freqProfs/scripts.js
+++ freqProfs/scripts.js
@@ -1,31 +1,47 @@
 const source = 'data.json';
+const display = document.querySelector("#display-data");
+const input = document.querySelector("#password");
+const btn = document.querySelector("#btnExe");
 
-function loginFreq() {
-    getData();
-    var senha = document.getElementById('password').value;
-    var font = source;
-    var result = font.filter(function(obj, index){
-        return obj.userID===senha;
-    })
-
-    console.log(result);
+const getData = async () => {
+    const res = await fetch(source);
+    const data = await res.json();
+    return data
 }
 
-function getData() {
-    fetch(source)
-    .then(rep => rep.json())
-    .then(data => {
-        outData(data);
-    })
-}
+const displayUsers = async () => {
+    let senha = input.value;
+    
+    const payload = await getData();
 
-function outData(val) {
-    console.log(val);
-    let html = '';
+    let dataDisplay = payload.filter((eventData) => {
+        if(senha === "") {
+            return ""
+        }
+        else if (senha === eventData.userID) {
+            return eventData
+        }
+    }).map((Object) => {
+        const {name, lastName, cargo, ueAtual, turma, periodo, anos} = Object;
 
-    val.forEach((ele) => {
-        console.log(ele);
-        html += `<h2 class="name" id="userName">${ele.name} <strong id="lastName">${ele.lastName}</strong></h2>`;
-    })
-    userName.innerHTML = html;
+        var contador = anos.split(',').filter((i) => i.length).length;
+        var anosTrab = anos.split(',');
+
+        return `
+        <p class="left-p">Olá</p>
+        <h2 class="name" id="userName"> ${name} <strong id="lastName"> ${lastName}</strong></h2>
+        <p class="p1">${cargo}</p>
+        <br>
+        <h2 class="heading-1">&nbsp;U.E. ATUAL</h2>
+        <p class="com-p"><i class="fa fa-building"></i> &nbsp;&nbsp;${ueAtual}</p>
+        <p class="com-p"><i class="fa fa-graduation-cap" aria-hidden="true"></i> &nbsp;${turma}</p>
+        <p class="com-p"><i class="fa fa-clock-o" aria-hidden="true"></i> &nbsp;&nbsp;${periodo}</p>
+        <br>
+        <h2 class="heading-1">&nbsp;ANO</h2>
+        <p class="com-p"><i class="fa fa-calendar"></i> &nbsp;&nbsp;ANO</p>
+        <select name="anos" class="com-i" id="anofreq">
+            <option value="${"1"}">${anosTrab[contador-1]}</option>
+        </select>`
+    }).join("");
+    display.innerHTML = dataDisplay;
 }
